7d68c9fdfa17e7c8cf96f48280b302e50eaeecca
PK006 <plai6588@gmail.com>
2023-04-08 14:02:39 +0700

    .

--- Home.py
+++ Home.py
@@ -6,4 +6,14 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.header("PK006")
+#st.header("PK006")
+#st.image("./pic/npru.jpg")
+html_8 = """
+<div style="background-color:#D89182;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+#st.image("./pic/cat.jpg")
