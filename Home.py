ad43adfac8e2d89b72a65f738ad40abe453af5d1
PK006 <plai6588@gmail.com>
2023-04-08 15:35:10 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -76,7 +76,7 @@ if st.button("ทำนายผล"):
    
    out=Knn_model.predict(x_input)
 
-    if out[0] == 'Setosa':
+if out[0] == 'Setosa':
     st.image("./pic/setosa.jpg")
     elif out[0] == 'versicolor':
     st.image("./pic/versicolor.jpg")
